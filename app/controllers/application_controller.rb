9754926539a27ebdf5a37bd908b8ae03b721dcae
sora2525 <sky0907a.a@gmail.com>
2024-11-04 19:46:16 +0900

    nameを保存可能に。Unpermitted警告解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
         include DeviseHackFakeSession
-end
+      
+        before_action :configure_permitted_parameters, if: :devise_controller?
+      
+        private
+      
+        def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        end
+      
+        def sign_in_params
+          params.permit(:email, :password)
+        end
+      
+        def sign_up_params
+          params.permit(:name, :email, :password, :password_confirmation)
+        end
+      
+        def account_update_params
+          params.permit(:name, :email, :password, :password_confirmation, :current_password)
+        end
+      end
+      
