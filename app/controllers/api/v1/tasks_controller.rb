7ddc1c4122d49f46d43723fc70cf8dfba45dc5ac
sora2525 <sky0907a.a@gmail.com>
2024-11-07 01:10:39 +0900

    current_userでタスク情報を取得

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,43 +1,49 @@
 class Api::V1::TasksController < ApplicationController
-    before_action :set_task, only: [:show,:update,:destroy]
-
+    include DeviseTokenAuth::Concerns::SetUserByToken
+    
+    # authenticate_user! の代わりに DeviseTokenAuth 用のメソッドを使用
+    before_action :authenticate_api_v1_user!
+    before_action :set_task, only: [:show, :update, :destroy]
+  
     def index
-        @tasks = Task.all
-        render json: @tasks
+      @tasks = current_api_v1_user.tasks 
+      render json: @tasks
     end
-
+  
     def show
-        render json: @task
+      render json: @task
     end
-
+  
     def create
-        @task = Task.new(task_params)
-        if @task.save
-          render json: @task, status: :created
-        else
-          render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
-        end
+      @task = current_api_v1_user.tasks.new(task_params) 
+      if @task.save
+        render json: @task, status: :created
+      else
+        render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
       end
+    end
+  
     def update
-        if @task.update(task_params)
-            render json: @task
-          else
-            render json: @task.errors, status: :unprocessable_entity
-          end
+      if @task.update(task_params)
+        render json: @task
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end
     end
-
+  
     def destroy
-        @task.destroy
-        head :no_content
+      @task.destroy
+      head :no_content
     end
-
+  
     private
-
+  
     def set_task
-        @task = Task.find(params[:id])
+      @task = current_api_v1_user.tasks.find(params[:id]) 
     end
-
+  
     def task_params
-        params.require(:task).permit(:title,:description,:due_date,:priority,:completion_date,:reminder_time,:user_id)
+      params.require(:task).permit(:title, :description, :due_date, :priority, :completion_date, :reminder_time)
     end
-end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_many :tasks, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
